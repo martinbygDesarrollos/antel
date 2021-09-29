69eca98c1f7151ad6ec1c7284e918a11a123a539
martinbygDesarrollos <75988208+martinbygDesarrollos@users.noreply.github.com>
2021-09-29 11:43:43 -0300

    Delete config.php

--- src/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define("DB_HOST", "127.0.0.1");
-define("DB_USR", "byguyqrd_admin_antel");
-define("DB_PASS", "bQ_K58JeCT-8");
-define("DB_DB", "byguyqrd_antel");
-define("URL_UTILS", '/home2/byguyqrd/utils/');
-?>
