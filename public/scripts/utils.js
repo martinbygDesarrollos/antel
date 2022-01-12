cfb91d284fcdaa2f93d18e919556f6cf76db9c45
Martin B&G Desarrollo de Sistemas <martin@hit.com.uy>
2022-01-12 17:12:04 -0300

    variable

--- public/scripts/utils.js
+++ public/scripts/utils.js
@@ -1,3 +1,6 @@
+//cuando se termina el proceso de envío de emails que notifica al celular phoneNotifications que se terminó el provceso
+var phoneNotifications = "92459188";
+
 function validateEmail(email) {
 	const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(String(email).toLowerCase());
