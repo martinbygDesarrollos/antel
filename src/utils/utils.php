795b652b45fc2c59f83dfbc05e15ede9dc1c31dc
Martin B&G Desarrollo de Sistemas <martin@hit.com.uy>
2022-06-20 09:35:59 -0300

    envio de whatsapp

--- src/utils/utils.php
+++ src/utils/utils.php
@@ -6,14 +6,29 @@ class utils{
 		//$data ejemplo "id=1&content=".$message."&to=598".$mobilePhone
 		$data .= "&token=".TOKEN_API;
 
-		$curlPetition = curl_init(URL_WHATSAPP_API.$path);
-		curl_setopt($curlPetition, CURLOPT_URL, URL_WHATSAPP_API.$path);
-		curl_setopt($curlPetition, CURLOPT_POST, true);
-		curl_setopt($curlPetition, CURLOPT_POSTFIELDS, $data);
-		curl_setopt($curlPetition, CURLOPT_RETURNTRANSFER, true);
-		$responseCurl =  curl_exec($curlPetition);
-		curl_close($curlPetition);
+		error_log(date("ymd His")." ".URL_WHATSAPP_API.$path." ".$data);
 
-		return json_decode($responseCurl);
+		$curl = curl_init();
+
+		curl_setopt_array($curl, array(
+		  CURLOPT_URL => URL_WHATSAPP_API.$path,//'https://sigecom.uy/middleware/public/txt',
+		  CURLOPT_RETURNTRANSFER => true,
+		  CURLOPT_ENCODING => '',
+		  CURLOPT_MAXREDIRS => 10,
+		  CURLOPT_TIMEOUT => 0,
+		  CURLOPT_FOLLOWLOCATION => true,
+		  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		  CURLOPT_CUSTOMREQUEST => 'GET',
+		  CURLOPT_POSTFIELDS => $data,//'id=1&content=mensaje%200919&to=92459188&token=45ek2wrhgr3rg33m',
+		  CURLOPT_HTTPHEADER => array(
+		    'Content-Type: application/x-www-form-urlencoded',
+		    'Cookie: PHPSESSID=9a1b26da908cb5b5a6a0c1049121a947'
+		  ),
+		));
+
+		$response = curl_exec($curl);
+
+		curl_close($curl);
+		return json_decode($response);
 	}
 }
